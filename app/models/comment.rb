c7d6259725a280c054c81be8708ccfe2aafe6fcc
siravijbb <siravijbb@gmail.com>
2024-11-03 14:11:35 +0700

    Finnished: comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
+  validates :commenter, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 10 }
   belongs_to :article
+
+
 end
